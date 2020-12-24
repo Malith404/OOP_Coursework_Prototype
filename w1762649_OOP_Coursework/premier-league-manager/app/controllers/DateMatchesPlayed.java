af01cd06573385123b08d4c846b16cd750108491
Oshadha Malith Goonathilake <malith.2018402@iit.ac.lk>
2020-12-24 20:07:03 +0530

    Fourth commit

--- w1762649_OOP_Coursework/premier-league-manager/app/controllers/DateMatchesPlayed.java
+++ w1762649_OOP_Coursework/premier-league-manager/app/controllers/DateMatchesPlayed.java
@@ -11,56 +11,6 @@ public class DateMatchesPlayed implements Serializable {
     private static Scanner dateValidation = new Scanner(System.in);
     public DateMatchesPlayed(int day, int month, int year) {
 
-//          try {
-//              if (day > 0 && day <= 31) {
-//                  this.day = day;
-//              } else {
-//                  System.out.print("Please Enter valid date which played the match: ");
-//                  setDay(dateValidation.nextInt());
-//              }
-//          } catch (Exception e) {
-//              System.out.print("Please enter valid Date of the match played: ");
-//              setDay(dateValidation.nextInt());
-//          }
-//
-//
-//            try {
-//              if (year == 2020) {
-//                    if (!(month == 8 || month == 9 || month == 10 || month == 11 || month == 12)) {
-//                        System.out.println("You have selected year 2020, therefore you are eligible to add to a season between 8-12(August-December)");
-//                        System.out.print("Please Enter valid month which played the match: ");
-//                        setMonth(dateValidation.nextInt());
-//                    } else {
-//                        this.month = month;
-//                    }
-//                }
-//                if (year == 2021) {
-//                    if (!(month == 1 || month == 2 || month == 3 || month == 4 || month == 5)) {
-//                        System.out.println("You have selected year 2021, therefore you are eligible to add to a season between 1-5(January-May)");
-//                        System.out.print("Please Enter valid month which played the match: ");
-//                        setMonth(dateValidation.nextInt());
-//                    } else {
-//                        this.month = month;
-//                    }
-//                }
-//            } catch (Exception e) {
-//                System.out.print("Please enter valid Month of the match played:");
-//                setMonth(dateValidation.nextInt());
-//            }
-//
-//
-//        try{
-//            if(year==2020 || year==2021){
-//                this.year = year;
-//            }else{
-//                System.out.print("Please Enter valid year which played the match: ");
-//                setYear(dateValidation.nextInt());
-//            }
-//        }catch (Exception e){
-//            System.out.print("Please enter Year of the match played :");
-//            setYear(dateValidation.nextInt());
-//        }
-
         try{
             if(day>0 && day<=31){
                 this.day = day;
@@ -114,19 +64,7 @@ public class DateMatchesPlayed implements Serializable {
 
     public void setDay(int day) {
         this.day=day;
-        /*try{
-            if(day>0 && day<=31){
-                this.day = day;
-            }else{
-                System.out.print("Please Enter valid date which played the match: ");
-                setDay(scan.nextInt());
-                System.out.println("\n");
-            }
-        }catch (Exception e){
-            System.out.print("Please enter valid date which played the match: ");
-            setDay(scan.nextInt());
-            System.out.println("\n");
-        }*/
+
     }
 
     public int getMonth() {
@@ -135,21 +73,7 @@ public class DateMatchesPlayed implements Serializable {
 
     public void setMonth(int month) {
         this.month=month;
-        /*try{
-            if(month>0 &&month<=12){
-                this.month = month;
-            }else{
-                System.out.print("Please Enter valid month which played the match:  ");
-                setMonth(scan.nextInt());
-                System.out.println("\n");
 
-            }
-        }catch (Exception e){
-            System.out.print("Please enter valid month which played the match: ");
-            setMonth(scan.nextInt());
-            System.out.println("\n");
-
-        }*/
     }
 
     public int getYear() {
@@ -158,21 +82,7 @@ public class DateMatchesPlayed implements Serializable {
 
     public void setYear(int year) {
         this.year=year;
-        /*try{
-            if(year==2020){
-                this.year = year;
-            }else{
-                System.out.print("Please Enter valid year which played the match: ");
-                setYear(scan.nextInt());
-                System.out.println("\n");
 
-            }
-        }catch (Exception e){
-            System.out.print("Please enter valid year which played the match: ");
-            setYear(scan.nextInt());
-            System.out.println("\n");
-
-        }*/
     }
 
     @Override
